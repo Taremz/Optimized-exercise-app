d659faaf4aea79cd9328f9da0f09e0b31254433b
Taremz <97131407+Taremz@users.noreply.github.com>
2022-01-12 23:28:33 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,23 +1,24 @@
 import healthsignup
 import height
 import weight
+
+
 def calculate_bmi():
     kg = int(weight_tf.get())
-    cm = int(height_tf.get())/100
-    bmi = kg/(cm*cm)
+    m = int(height_tf.get())/100
+    bmi = kg/(m*m)
     bmi = round(bmi, 1)
     bmi_index(bmi)
 
-
-if BMI <= 18.5:
-    print("underweight.")
-elif BMI <= 24.9:
-    print(" healthy.")
-elif BMI <= 29.9:
-    print("over weight.")
-elif BMI <= 34.9:
-    print("severely over weight.")
-elif BMI <= 39.9:
-    print(" obese.")
-else:
-    print(" severely obese.")
+def bmi_index(bmi):
+    
+    if bmi < 18.5:
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Underweight')
+    elif (bmi > 18.5) and (bmi < 24.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Normal')
+    elif (bmi > 24.9) and (bmi < 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Overweight')
+    elif (bmi > 29.9):
+        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Obese') 
+    else:
+        messagebox.showerror('bmi-pythonguides', 'something went wrong!')   
