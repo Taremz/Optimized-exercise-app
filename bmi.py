1428fb63858ea092f938b3cc9792a33c2ce9e1de
Taremz <97131407+Taremz@users.noreply.github.com>
2022-01-13 16:40:59 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -22,3 +22,50 @@ def bmi_index(bmi):
         messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Obese') 
     else:
         messagebox.showerror('bmi-pythonguides', 'something went wrong!')   
+
+height_lb.grid(row=3, column=1)
+
+weight_lb = Label(
+    frame,
+    text="Enter Weight (kg)  ",
+
+)
+weight_lb.grid(row=4, column=1)
+
+height_tf = Entry(
+    frame,
+)
+height_tf.grid(row=3, column=2, pady=5)
+
+weight_tf = Entry(
+    frame,
+)
+weight_tf.grid(row=4, column=2, pady=5)
+
+frame3 = Frame(
+    frame
+)
+frame3.grid(row=5, columnspan=3, pady=10)
+
+cal_btn = Button(
+    frame3,
+    text='Calculate',
+    command=calculate_bmi
+)
+cal_btn.pack(side=LEFT)
+
+reset_btn = Button(
+    frame3,
+    text='Reset',
+    command=reset_entry
+)
+reset_btn.pack(side=LEFT)
+
+exit_btn = Button(
+    frame3,
+    text='Exit',
+    command=lambda:ws.destroy()
+)
+exit_btn.pack(side=RIGHT)
+
+ws.mainloop()
